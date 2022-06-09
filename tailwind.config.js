02af641029e666968fd1af8029b01b5cc7ab65c9
CcharlotteHh <210547@student.glu.nl>
2022-06-09 12:15:49 +0200

    added some things

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,2 @@
 module.exports = {
-  content: ['./*html'],
-  theme: {
-    screens :{
-      sm:'480px',
-      md:'768px',
-      lg:'976px',
-      xl:'1440px',
-    },
-    extend: {
-      colors:{
-        brightRed: 'hsl(12, 88%, 59%)',
-        brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSupLight: 'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(233, 12%, 13%)',
-        veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)',
-        lightSlateGray: 'hsl(209,13%,51%)',
-        testPurple : 'hsl(244,57%,50%)',
-        testLilac: 'hsl(272,49%,69%)'
-      }
-      
-    },
-  },
-  plugins: [],
-}
+  content: ['./*html']}
